0c446bf4ba25b5044a5ac5bf0c244af8f92927dd
deviltry <ndisposable@gmail.com>
2022-04-18 18:27:56 +0300

    mouse move update

--- Loatheb/Program.cs
+++ Loatheb/Program.cs
@@ -1,33 +1,75 @@
 ﻿using System.Diagnostics;
-using Loatheb;
+using Loatheb.win32;
 
 Console.WriteLine("Press any key for movement test");
 var rnd = new Random();
 
-Win32.POINT p = new Win32.POINT(100, 100);
-
 var handle = GetWindowHandle();
+
+var blaX = Win32Api.GetSystemMetrics(Structures.SystemMetric.SM_CXSCREEN);
+var blaY = Win32Api.GetSystemMetrics(Structures.SystemMetric.SM_CYSCREEN);
+
+int MOUSE_INPUT_BATCH = 5;
+int MOUSE_SLEEP_CHANCE = 25;
+
+var inputs = new Structures.INPUT[MOUSE_INPUT_BATCH];
+for (int i = 0; i < MOUSE_INPUT_BATCH; i++)
+{
+	inputs[i] = new Structures.INPUT();
+}
+
+var mouseInput = new Structures.MOUSEINPUT();
 
 while (Console.ReadKey().Key != ConsoleKey.X)
 {
-    for (var i = 0; i < 50000; i++)
-    {
-        p.x = i;
-        Console.WriteLine(i);
-        Win32.mouse_event((int)(Win32.MouseEventFlags.MOVE | Win32.MouseEventFlags.ABSOLUTE), p.x, p.y, 0, 0);
-        // Win32.ClientToScreen(handle, ref p);
-        // Win32.SetCursorPos(p.x, p.y);
-        if (i % rnd.Next(500, 1000) == 0)
-            Thread.Sleep(1);
-        i += rnd.Next(5, 10);
-    }
+	for (var i = 0; i < blaY - 100; i++)
+	{
+		for (int j = 0; j < inputs.Length; j++)
+		{
+			inputs[j].type = Structures.INPUT_MOUSE;
+			mouseInput.dy = ConvertAbsoluteCoordinate(i, blaY);
+			i += rnd.Next(1, 5);
+			mouseInput.dx = 100;
+			mouseInput.dwFlags = Structures.MOUSEEVENTF.MOVE | Structures.MOUSEEVENTF.ABSOLUTE;
+			inputs[j].U = new Structures.InputUnion {mi = mouseInput};
+		}
 
-    p.x = 100;
-    p.y = 100;
+		if (rnd.Next(100) < MOUSE_SLEEP_CHANCE)
+			Thread.Sleep(1);
+
+		// Console.WriteLine(i);
+		Win32Api.SendInput((uint) inputs.Length, inputs, Structures.INPUT.Size);
+	}
+
+	Console.ReadKey();
+
+	for (var i = 0; i < blaX - 100; i++)
+	{
+		for (int j = 0; j < inputs.Length; j++)
+		{
+			inputs[j].type = Structures.INPUT_MOUSE;
+			mouseInput.dx = ConvertAbsoluteCoordinate(i, blaX);
+			i += rnd.Next(1, 5);
+			mouseInput.dy = 100;
+			mouseInput.dwFlags = Structures.MOUSEEVENTF.MOVE | Structures.MOUSEEVENTF.ABSOLUTE;
+			inputs[j].U = new Structures.InputUnion {mi = mouseInput};
+		}
+
+		if (rnd.Next(100) < MOUSE_SLEEP_CHANCE)
+			Thread.Sleep(1);
+
+		// Console.WriteLine(i);
+		Win32Api.SendInput((uint) inputs.Length, inputs, Structures.INPUT.Size);
+	}
 }
 
 IntPtr GetWindowHandle()
 {
-    return Process.GetCurrentProcess().Handle;
-    // return Process.GetProcesses().FirstOrDefault(x => x.ProcessName.ToLower().StartsWith("notepad"))?.Handle ?? IntPtr.Zero;
+	return Process.GetCurrentProcess().Handle;
+	// return Process.GetProcesses().FirstOrDefault(x => x.ProcessName.ToLower().StartsWith("notepad"))?.Handle ?? IntPtr.Zero;
+}
+
+int ConvertAbsoluteCoordinate(int coordinate, int resolution)
+{
+	return ((65536 * coordinate) / resolution) + 1;
 }
---
+++ Loatheb/win32/Win32Api.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Loatheb.win32;
+
+internal static class Win32Api
+{
+	[DllImport("user32.dll")]
+	internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] Structures.INPUT[] pInputs, int cbSize);
+
+	[DllImport("User32.dll")]
+	internal static extern int GetSystemMetrics(Structures.SystemMetric smIndex);
+}
