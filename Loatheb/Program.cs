aad2bebd5ef29ed900c0cc0d06d48ea8ef4ed5a1
deviltry <ndisposable@gmail.com>
2022-04-18 16:19:19 +0300

    mouse move test

---
+++ Loatheb/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Loatheb;
+
+Console.WriteLine("Press any key for movement test");
+var rnd = new Random();
+
+Win32.POINT p = new Win32.POINT(100, 100);
+
+var handle = GetWindowHandle();
+
+while (Console.ReadKey().Key != ConsoleKey.X)
+{
+    for (var i = 0; i < 50000; i++)
+    {
+        p.x = i;
+        Console.WriteLine(i);
+        Win32.mouse_event((int)(Win32.MouseEventFlags.MOVE | Win32.MouseEventFlags.ABSOLUTE), p.x, p.y, 0, 0);
+        // Win32.ClientToScreen(handle, ref p);
+        // Win32.SetCursorPos(p.x, p.y);
+        if (i % rnd.Next(500, 1000) == 0)
+            Thread.Sleep(1);
+        i += rnd.Next(5, 10);
+    }
+
+    p.x = 100;
+    p.y = 100;
+}
+
+IntPtr GetWindowHandle()
+{
+    return Process.GetCurrentProcess().Handle;
+    // return Process.GetProcesses().FirstOrDefault(x => x.ProcessName.ToLower().StartsWith("notepad"))?.Handle ?? IntPtr.Zero;
+}
---
+++ Loatheb/Win32.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+namespace Loatheb;
+
+public class Win32
+{
+    [DllImport("User32.Dll")]
+    public static extern long SetCursorPos(int x, int y);
+
+    [DllImport("User32.dll")]
+    public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+    [DllImport("User32.Dll")]
+    public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+
+        public POINT(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+
+    [Flags]
+    public enum MouseEventFlags
+    {
+        LEFTDOWN = 0x00000002,
+        LEFTUP = 0x00000004,
+        MIDDLEDOWN = 0x00000020,
+        MIDDLEUP = 0x00000040,
+        MOVE = 0x00000001,
+        ABSOLUTE = 0x00008000,
+        RIGHTDOWN = 0x00000008,
+        RIGHTUP = 0x00000010
+    }
+
+}
